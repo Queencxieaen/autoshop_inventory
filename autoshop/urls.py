a7f8e6255990f7e237b8fd96bdad9ee18b05d835
Queency Ann Lastimado <lastiimadoqueencyann01@gmail.com>
2026-03-23 16:04:58 +0800

    Fix urls dashboard

--- autoshop/urls.py
+++ autoshop/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # DASHBOARD
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
     # path('', include(tf_urls)),
 
